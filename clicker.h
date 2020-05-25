7fb263a0aa08134c22b9b865e0a3e407aef3957e
Nusrat-16 <55928860+Nusrat-16@users.noreply.github.com>
2020-05-25 22:47:28 +0200

    Dynamic Rpl Protocol (Multihoping)

---
+++ clicker.h
@@ -0,0 +1,11 @@
+#ifndef _CLICKER_H
+#define _CLICKER_H
+
+/* Rime channel used for this application. */
+#define CLICKER_CHANNEL 130
+/* IEEE 802.15.4 channel used for this application. */
+#define IEEE802_15_4_CHANNEL 16
+/* Transmission power for this application. */ /* Default is 3 */
+#define CC2420_TX_POWER 25
+
+#endif
